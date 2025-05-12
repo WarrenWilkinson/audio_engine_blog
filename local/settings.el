70c9489cfc79e519360a1c59dfe9c6c8134285ce
Warren Wilkinson <warrenwilkinson@gmail.com>
2025-05-12 17:45:54 -0600

    First Commit

---
+++ local/settings.el
@@ -0,0 +1,39 @@
+
+(setq org-static-blog-domain "file:///home/warren/projects/audio_engine_blog/");; https://staticblog.org/
+
+(setq org-static-blog-publish-title "My Static Org Blog")
+(setq org-static-blog-publish-url org-static-blog-domain)
+(setq org-static-blog-publish-directory "~/projects/audio_engine_blog/")
+(setq org-static-blog-posts-directory "~/projects/audio_engine_blog/posts/")
+(setq org-static-blog-drafts-directory "~/projects/audio_engine_blog/drafts/")
+(setq org-static-blog-enable-tags t)
+(setq org-export-with-toc nil)
+(setq org-export-with-section-numbers nil)
+
+(setq org-static-blog-page-header
+      "<meta name=\"author\" content=\"Warren Wilkinson\">
+<meta name=\"referrer\" content=\"no-referrer\">
+<meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">
+<link href= \"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+<link rel=\"icon\" href=\"static/favicon.ico\">")
+
+;; This preamble is inserted at the beginning of the <body> of every page:
+;;   This particular HTML creates a <div> with a simple linked headline
+(setq org-static-blog-page-preamble
+      (concat "<div class=\"header\">
+  <a href=\"" org-static-blog-domain "index.html\">My Static Org Blog</a>
+</div>"))
+
+;; This postamble is inserted at the end of the <body> of every page:
+;;   This particular HTML creates a <div> with a link to the archive page
+;;   and a licensing stub.
+(setq org-static-blog-page-postamble
+      (concat "<div id=\"archive\">
+  <a href=\"" org-static-blog-domain "archive.html\">Other posts</a>
+</div>
+<center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"https://purl.org/dc/terms/\" href=\"https://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by <a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"https://bastibe.de\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Bastian Bechtold</a> is licensed under a <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>"))
+
+;; This HTML code is inserted into the index page between the preamble and
+;;   the blog posts
+(setq org-static-blog-index-front-matter
+      "<h1> Welcome to my blog </h1>\n")
