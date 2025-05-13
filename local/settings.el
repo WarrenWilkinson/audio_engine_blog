d795e6661f310848c6afc6489a5afa40def89fad
Warren Wilkinson <warrenwilkinson@gmail.com>
2025-05-13 11:39:01 -0600

    Put readme at top, add license to content

--- local/settings.el
+++ local/settings.el
@@ -27,11 +27,19 @@
 ;; This postamble is inserted at the end of the <body> of every page:
 ;;   This particular HTML creates a <div> with a link to the archive page
 ;;   and a licensing stub.
+;; (setq org-static-blog-page-postamble
+;;       (concat "<div id=\"archive\">
+;;   <a href=\"" org-static-blog-domain "archive.html\">Other posts</a>
+;; </div>
+;; <center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/4.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/4.0/88x31.png\" /></a><br /><span xmlns:dct=\"https://purl.org/dc/terms/\" href=\"https://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">" org-static-blog-domain "</span> by <a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"" org-static-blog-domain "\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Warren Wilkinson</a> is licensed under a <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/4.0/\">Creative Commons Attribution-ShareAlike 4.0 Unported License</a>.</center>"))
+
 (setq org-static-blog-page-postamble
-      (concat "<div id=\"archive\">
-  <a href=\"" org-static-blog-domain "archive.html\">Other posts</a>
-</div>
-<center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"https://purl.org/dc/terms/\" href=\"https://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by <a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"https://bastibe.de\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Bastian Bechtold</a> is licensed under a <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>"))
+      (concat
+;       "<div id=\"archive\">
+;  <a href=\"" org-static-blog-domain "archive.html\">Other posts</a>
+; </div>
+"<center>
+<a ref=\"license\" href=\"" org-static-blog-domain "\">"org-static-blog-domain"</a> © 2025 by Warren Wilkinson is licensed under <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\">CC BY-SA 4.0</a><img src=\"https://mirrors.creativecommons.org/presskit/icons/cc.svg\" style=\"max-width: 1em;max-height:1em;margin-left: .2em;\"><img src=\"https://mirrors.creativecommons.org/presskit/icons/by.svg\" style=\"max-width: 1em;max-height:1em;margin-left: .2em;\"><img src=\"https://mirrors.creativecommons.org/presskit/icons/sa.svg\" style=\"max-width: 1em;max-height:1em;margin-left: .2em;\">"))
 
 ;; This HTML code is inserted into the index page between the preamble and
 ;;   the blog posts
